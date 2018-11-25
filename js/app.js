d8b8cfc4e3181c877e768e25c0e7b4415470056a
chmezac <ch.mezac@alumnos.duoc.cl>
2018-11-25 18:02:11 -0400

    Se eliminan elementos relacionados a iOS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
-( function() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-                 .register('./service-worker.js')
-                 .then(function() { 
-                     console.log('Service Worker Registered'); 
-                });
-      }
-} )( );
+//( function() {
+//    if ('serviceWorker' in navigator) {
+//        navigator.serviceWorker
+//                 .register('./service-worker.js')
+//                 .then(function() { 
+//                     console.log('Service Worker Registered'); 
+//                });
+//      }
+//} )( );
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function() {
+        console.log('Service Worker Registered');
+      });
+  }
