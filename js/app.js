273d8c49a82f018f492d5cfaf128d10ca2ee223c
huzu513 <huzumymw513@gmail.com>
2018-11-24 23:08:30 -0400

    Quita elementos para sistemas iOS

--- js/app.js
+++ js/app.js
@@ -8,17 +8,3 @@
       }
 } )( );
 
-
-//iOS
-//componentDidMount() {
-//    window.addEventListener('online', () => this.setOnlineStatus(true));
-//   window.addEventListener('offline', () => this.setOnlineStatus(false));
-//  }
-
-//  componentWillUnmount() {
-//    window.removeEventListener('online');
-//    window.removeEventListener('offline');
-//  }
-
-//setOnlineStatus = isOnline => this.setState({ online: isOnline })
-
