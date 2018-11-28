dcb7251cc8be6ee823a056abd06ed7f34380b4af
chmezac <ch.mezac@alumnos.duoc.cl>
2018-11-27 22:34:35 -0400

    Agrega registro.html

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 
     var loadData = function() {
         var xhttp = new XMLHttpRequest();
-        var url = "http://127.0.0.1:8000/perris/";
+        var url = "http://testperris3.pythonanywhere.com/perris/";
 
         xhttp.onreadystatechange = function() {
             if( this.readyState == 4 && this.status == 200 ){
